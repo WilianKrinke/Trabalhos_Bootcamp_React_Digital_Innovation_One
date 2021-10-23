97843f8cb3b1895a933d5bb35360257f4250d3fc
Wilian Krinke <krinkewilian@gmail.com>
2021-10-23 00:36:22 -0300

    testes

--- Redux/src/store/middlewares/middlewares.js
+++ Redux/src/store/middlewares/middlewares.js
@@ -1,3 +1,11 @@
 export function invertSentence(quote) {
-    return quote.split("").reverse().join("");;
+    const noEmptyQuote = noEmpty(quote)
+    return noEmptyQuote.split("").reverse().join("");
+}
+
+function noEmpty(quote) {
+    if (quote.length === 0) {
+        return "Preencha o campo corretamente"
+    }
+    return quote;
 }
