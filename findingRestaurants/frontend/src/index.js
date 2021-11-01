2bdaa54ddff0c1167dc3e464067a5a1a82fe4133
Wilian Krinke <krinkewilian@gmail.com>
2021-11-01 00:47:57 -0300

    alterações na barra de rolagem horizontal

--- findingRestaurants/frontend/src/index.js
+++ findingRestaurants/frontend/src/index.js
@@ -5,6 +5,7 @@ import '@material/react-text-field/dist/text-field.css';
 import '@material/react-material-icon/dist/material-icon.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import './index.css'
 
 import App from './App';
 import store from './redux/store';
