383ae27192b371ed63193fac2169d3a66d78aada
Wilian Krinke <krinkewilian@gmail.com>
2022-01-07 19:52:57 -0300

    pequenas anotações

--- Redux/src/components/Counter/Counter.jsx
+++ Redux/src/components/Counter/Counter.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from '../../store/actions/actions';
-import './counter.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../../store/actions/actions";
+import "./counter.css";
 
+const Counter = () => {
+  const number = useSelector((state) => state.counterNumber.number); //pegar e exibir estado
+  const dispatch = useDispatch(); //acao do estado
 
-const Counter = () => {  
-    
-    const number = useSelector((state) => state.counterNumber.number)
-    const dispatch = useDispatch()
-    
-    
-    return (
-        <>
-            <section className="container_counter">
-                <button onClick={e => dispatch(decrement())}> - </button>
+  return (
+    <>
+      <section className="container_counter">
+        <button onClick={(e) => dispatch(decrement())}> - </button>
 
-                <div>
-                    <h2>{number}</h2>
-                    
-                </div>
+        <div>
+          <h2>{number}</h2>
+        </div>
 
-                <button onClick={e => dispatch(increment())}> + </button>
-            </section>
-        </>
-    );
-}
+        <button onClick={(e) => dispatch(increment())}> + </button>
+      </section>
+    </>
+  );
+};
 
 export default Counter;
--- Redux/src/pages/secondpage/SecondPage.jsx
+++ Redux/src/pages/secondpage/SecondPage.jsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import Breadcrumb from 'react-bootstrap/Breadcrumb'
-import Counter from '../../components/Counter/Counter'
-import { useSelector } from 'react-redux';
-import { FooterStyled, HeaderStyled, MainStyled } from '../styled';
-import { Link } from 'react-router-dom';
-import Switchdark from '../../components/SwitchDark/SwitchDark';
+import React from "react";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+import Counter from "../../components/Counter/Counter";
+import { useSelector } from "react-redux";
+import { FooterStyled, HeaderStyled, MainStyled } from "../styled";
+import { Link } from "react-router-dom";
+import Switchdark from "../../components/SwitchDark/SwitchDark";
 
 const Secondpage = () => {
+  const numero = useSelector((state) => state.counterNumber.number);
+  const toggleState = useSelector((state) => state.turnOffOrOn.on);
 
-    const numero = useSelector((state) => state.counterNumber.number)
-    const toggleState = useSelector((state) => state.turnOffOrOn.on)
+  return (
+    <>
+      <HeaderStyled className="flex" toggleState={toggleState}>
+        <h1>Second Page</h1>
+        <Breadcrumb>
+          <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
 
-    return (
-        <>
-            <HeaderStyled className="flex" toggleState={toggleState}>
-                <h1>Second Page</h1>
-                <Breadcrumb>
-                    <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+          <Breadcrumb.Item active>Second Page</Breadcrumb.Item>
 
-                    <Breadcrumb.Item active>Second Page</Breadcrumb.Item>
-
-                    <Breadcrumb.Item href="/thirdypage">Third Page</Breadcrumb.Item>
-                </Breadcrumb>
-            </HeaderStyled>
-            <MainStyled toggleState={toggleState}>
-                <Counter />
-            </MainStyled>
-            <FooterStyled toggleState={toggleState}>
-                <h1>{numero}</h1>
-                <Link to="/thirdypage">Third Page</Link>
-                <Switchdark />
-            </FooterStyled>
-        </>
-    );
-}
+          <Breadcrumb.Item href="/thirdypage">Third Page</Breadcrumb.Item>
+        </Breadcrumb>
+      </HeaderStyled>
+      <MainStyled toggleState={toggleState}>
+        <Counter />
+      </MainStyled>
+      <FooterStyled toggleState={toggleState}>
+        <h1>{numero}</h1>
+        <Link to="/thirdypage">Third Page</Link>
+        <Switchdark />
+      </FooterStyled>
+    </>
+  );
+};
 
 export default Secondpage;
