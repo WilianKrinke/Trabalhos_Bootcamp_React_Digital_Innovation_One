c5a7af3208070275d3f2b9d8f7b7051596016edd
Wilian Krinke <krinkewilian@gmail.com>
2022-01-09 19:34:50 -0300

    pequenas anotações

--- Redux/src/components/SwitchDark/SwitchDark.jsx
+++ Redux/src/components/SwitchDark/SwitchDark.jsx
@@ -1,32 +1,30 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { toggleDarktoLight } from '../../store/actions/actions';
-import { SpanStyled, SwitchButtonStyled } from './style';
-import Switch from '@mui/material/Switch';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { toggleDarktoLight } from "../../store/actions/actions";
+import { SpanStyled, SwitchButtonStyled } from "./style";
+import Switch from "@mui/material/Switch";
 
 const Switchdark = () => {
-    
-    const toggleState = useSelector(state => state.turnOffOrOn.on)
-    const dispatch = useDispatch()
+  const toggleState = useSelector((state) => state.turnOffOrOn.on); //state+reducer+inicialstate
+  const dispatch = useDispatch();
 
-    function handleChange() {
-        dispatch(toggleDarktoLight())
-    }
+  function handleChange() {
+    dispatch(toggleDarktoLight());
+  }
 
-
-    return (
-        <>            
-            <SwitchButtonStyled onClick={handleChange} toggleState={toggleState}>
-                <div></div>
-            </SwitchButtonStyled>
-            <SpanStyled toggleState={toggleState}>
-                {toggleState ? "Light":"Dark"}
-            </SpanStyled >
-            <br />
-            <Switch defaultChecked/>       
-        </>
-    );
-}
+  return (
+    <>
+      <SwitchButtonStyled onClick={handleChange} toggleState={toggleState}>
+        <div></div>
+      </SwitchButtonStyled>
+      <SpanStyled toggleState={toggleState}>
+        {toggleState ? "Light" : "Dark"}
+      </SpanStyled>
+      <br />
+      <Switch defaultChecked />
+    </>
+  );
+};
 
 export default Switchdark;
